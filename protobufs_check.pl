d22f0b9f84d96fb569031e5cf52288ada048f7b9
Jeff Rosenwald <JeffRose@acm.org>
2010-04-30 07:30:12 -0400

    Fix-ups for errored test case cases for make-check

--- protobufs_check.pl
+++ protobufs_check.pl
@@ -12,8 +12,7 @@
 ~>(P, Q) :-
 	setup_call_cleanup(P, (true; fail), assertion(Q)).
 
-enum(List, Element, Index) :-
-       nth1(Index, List, Element).
+:- set_prolog_flag(backquoted_string, true).
 
 protobufs:nested_enum(Key, Value) :-
 	nested_enum(Key, Value).
@@ -47,8 +46,8 @@ golden_message(Proto) :-
 			   integer64(8, 108),
 			   integer32(9, 109),
 			   integer64(10, 110),
-			   float(11, 111),
-			   double(12, 112),
+			   float(11, 111.0),
+			   double(12, 112.0),
 			   boolean(13, true),
 			   atom(14, '115'),
 			   codes(15, "116"),
@@ -59,8 +58,8 @@ golden_message(Proto) :-
 			   enum(21, nested_enum(baz)),    % nested_enum  BAZ
 			   enum(22, foreign_enum(baz)),     % nested_enum  FOREIGN_BAZ
 			   enum(23, import_enum(baz)),     %  nested_enum IMPORT_BAZ
-			   string(24, "124"),   % string_piece
-			   string(25, "125"),   % cord
+			   string(24, `124`),   % string_piece
+			   string(25, `125`),   % cord
 			   repeated(31, unsigned([201, 301])),
 			   repeated(32, unsigned([202, 302])),
 			   repeated(33, unsigned([203, 303])),
@@ -98,7 +97,7 @@ golden_message(Proto) :-
 			   float(71, 411.0),
 			   double(72, 412.0),
 			   boolean(73, false),
-			   string(74, "415"),
+			   string(74, `415`),
 			   codes(75, "416"),
 			   enum(81, nested_enum(foo)),
 			   enum(82, foreign_enum(foo)),
@@ -178,11 +177,6 @@ golden_message_template(Proto) :-
 			   codes(_, _)
 			 ]).
 
-verify_golden_message(Proto, Residue) :-
-	read_file_to_codes('./golden_message', Codes, [type(binary)]), !,
-
-	protobuf_message(Proto, Codes, Residue).
-
 check :-
 	golden_message(Message),
 	golden_message_template(Template),
@@ -190,34 +184,44 @@ check :-
 
 	read_file_to_codes('./golden_message', Codes, [type(binary)]),
 
-	write('unifying canned Golden Message with canned golden template...')
+	write('Unifying canned Golden Message with canned Golden Template...')
 	  ~> (Test1 == ok -> writeln('OK'); writeln('FAILED')),
 
 	  (   (Message = Template, Message == Template) -> Test1 = ok), !,
 
-	write('unifying canned message with Google''s Golden Message...')
+	write('Unifying canned Golden Message with Google''s Golden Message...')
 	  ~> (Test2 == ok -> writeln('OK'); writeln('FAILED')),
 
-	(   verify_golden_message(Message, []) -> Test2 = ok), !,
+	(   protobuf_message(Message, Codes) -> Test2 = ok), !,
 
-	write('unifying canned template with Google''s Golden Message...')
+	write('Unifying canned Golden Template with Google''s Golden Message...')
 	  ~> (Test3 == ok -> writeln('OK'); writeln('FAILED')),
 
-	(   verify_golden_message(Template, []) -> Test3 = ok), !,
+	(   protobuf_message(Template, Codes) -> Test3 = ok), !,
 
-	write('serializing canned Golden Message to Codes...')
+	write('Serializing canned Golden Message to Codes...')
 	  ~> (Test4 == ok -> writeln('OK'); writeln('FAILED')),
 
-	(   (   protobuf_message(Message, Serialized), Serialized == Codes)
+	(   protobuf_message(Message, Serialized )
 	-> Test4 = ok), !,
 
-	write('parsing Codes to canned Golden Template...')
+	write('Comparing Google''s Golden Message to Codes...')
+	  ~> (Test4a == ok -> writeln('OK'); writeln('FAILED')),
+
+	(   Serialized == Codes
+	-> Test4a = ok), !,
+
+	write('Parsing Codes to canned Golden Template...')
 	  ~> (Test5 == ok -> writeln('OK'); writeln('FAILED')),
 
-	(   (   protobuf_message(Template1, Serialized), Message == Template1)
-	-> Test5 = ok), !,
+	(   protobuf_message(Template1, Serialized) -> Test5 = ok), !,
 
-	write('all tests passed~n').
+	write('Comparing canned Golden Message to parsed Golden Template...')
+	  ~> (Test6 == ok -> writeln('OK'); writeln('FAILED')),
+
+	(   (Message == Template1) -> Test6 = ok), !,
+
+	writeln('All tests passed.').
 
 protobuf_check :-
 	catch(check, Err, (print_message(error, Err), fail)),
@@ -225,75 +229,4 @@ protobuf_check :-
 
 protobuf_check :-
 	halt(1).
-
-
-vector_type(double(_List), 2).
-vector_type(float(_List), 3).
-vector_type(integer(_List), 4).
-vector_type(integer64(_List), 5).
-vector_type(integer32(_List), 6).
-vector_type(unsigned(_List), 7).
-vector_type(codes(_List), 8).
-vector_type(atom(_List), 9).
-vector_type(string(_List), 10).
-
-
-vector(Type, B):-
-	vector_type(Type, Tag),
-
-	Proto = protobuf([ repeated(Tag, Type) ]),
-
-	protobuf_message(Proto, B).
-
-testv(N) :-
-	numlist(-20, N, X),
-	vector(double(X), Z),
-
-	open('tmp99.tmp', write, S, [type(binary)])
-	   ~> close(S),
-
-	format(S, '~s', [Z]), !.
-
-
-testv1(V) :-
-	read_file_to_codes('tmp99.tmp', Codes, [type(binary)]),
-
-	vector(V, Codes).
-
-commands(square, 1).
-commands(decimate, 2).
-commands(transform, 3).
-commands(inverse_transform, 4).
-
-basic_vector(Type, Proto) :-
-	vector_type(Type, Tag),
-
-	Proto = protobuf([ repeated(Tag, Type) ]).
-
-send_command(Command, Vector, Msg) :-
-
-	basic_vector(Vector, Proto1),
-
-	Proto = protobuf([enum(1, commands(Command)), embedded(2, Proto1)]),
-
-	protobuf_message(Proto, Msg).
-
-
-compound_protobuf(complex(Real, Img), group(12, [double(1, Real), double(2, Img)])).
-compound_protobuf(float(Val), float(13, Val)).
-compound_protobuf(double(Val), double(14, Val)).
-compound_protobuf((Num rdiv Den), group(15, [integer(1, Num), integer(2, Den)])).
-compound_protobuf(integer(Val), integer(16, Val)).
-
-
-protobuf_bag([], []).
-
-protobuf_bag([ Type | More], Msg) :-
-
-	compound_protobuf(Type, Proto),
-
-	protobuf_message(protobuf([Proto]), Msg, Msg1),
-
-	protobuf_bag(More, Msg1), !.
-
 
