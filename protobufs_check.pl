fce923896818f354b7f3e8bf2821faf097aa8003
Jeff Rosenwald <JeffRose@acm.org>
2010-05-03 07:38:13 -0400

    Fix-ups to add error if golden wirestream can't be found

--- protobufs_check.pl
+++ protobufs_check.pl
@@ -1,3 +1,32 @@
+/*  Part of SWI-Prolog
+
+    Author:        Jeffrey Rosenwald
+    E-mail:        jeffrose@acm.org
+    WWW:           http://www.swi-prolog.org
+    Copyright (C): 2010, Jeffrey Rosenwald
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    As a special exception, if you link this library with other files,
+    compiled with a Free Software compiler, to produce an executable, this
+    library does not by itself cause the resulting executable to be covered
+    by the GNU General Public License. This exception does not however
+    invalidate any other reasons why the executable file might be covered by
+    the GNU General Public License.
+*/
+
 :- module(protobufs_check,
 	[ check/0,
 	  protobuf_check/0
@@ -6,7 +35,6 @@
 :- use_module(library(protobufs)).
 
 :- meta_predicate ~>(0,0).
-
 :- op(950, xfy, ~>).
 
 ~>(P, Q) :-
@@ -183,7 +211,12 @@ check :-
 	copy_term(Template, Template1),
 	copy_term(Template, Template2),
 
-	read_file_to_codes('./golden_message', Wirestream, [type(binary)]),
+
+	write('Loading Google''s Golden Wirestream...')
+	  ~> (Test1a == ok -> writeln('OK'); writeln('FAILED')),
+
+	  (    read_file_to_codes('./golden_message', Wirestream, [type(binary)])
+	  -> Test1a = ok), !,
 
 	write('Unifying canned Golden Message with canned Golden Template...')
 	  ~> (Test1 == ok -> writeln('OK'); writeln('FAILED')),
