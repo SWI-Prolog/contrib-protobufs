984d8fef06db00fdb45d3c52a6d769c16024e02d
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2018-11-26 11:02:12 +0100

    Added WORDS_BIGENDIAN

--- config.h.cmake
+++ config.h.cmake
@@ -1,4 +1,5 @@
 #cmakedefine HAVE_SYS_TYPES_H @HAVE_SYS_TYPES_H@
-#cmakedefine O_PLMT @O_PLMT@
+#cmakedefine O_PLMT           @O_PLMT@
 #cmakedefine PL_ARITY_AS_SIZE @PL_ARITY_AS_SIZE@
-#cmakedefine _REENTRANT @_REENTRANT@
+#cmakedefine _REENTRANT       @_REENTRANT@
+#cmakedefine WORDS_BIGENDIAN  @WORDS_BIGENDIAN@
