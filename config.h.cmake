a7bca5bc136fc2f63423afc4c51998225fbc7cca
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2020-04-15 10:17:38 +0200

    Removed PL_ARITY_AS_SIZE

--- config.h.cmake
+++ config.h.cmake
@@ -1,5 +1,4 @@
 #cmakedefine HAVE_SYS_TYPES_H @HAVE_SYS_TYPES_H@
 #cmakedefine O_PLMT           @O_PLMT@
-#cmakedefine PL_ARITY_AS_SIZE @PL_ARITY_AS_SIZE@
 #cmakedefine _REENTRANT       @_REENTRANT@
 #cmakedefine WORDS_BIGENDIAN  @WORDS_BIGENDIAN@
